0a8fc85de13efdd97901cab82227f9ae54cbfa42
Ben Rodenkirch <brodenkirch@wisc.edu>
2023-03-15 23:13:18 -0500

    Debugging and Defensive Programming

--- inflammation/models.py
+++ inflammation/models.py
@@ -2,8 +2,8 @@
 
 The Model layer is responsible for the 'business logic' part of the software.
 
-Patients' data is held in an inflammation table (2D array) where each row contains 
-inflammation data for a single patient taken over a number of days 
+Patients' data is held in an inflammation table (2D array) where each row contains
+inflammation data for a single patient taken over a number of days
 and each column represents a single day across all patients.
 """
 
@@ -20,7 +20,8 @@ def load_csv(filename):
 
 def daily_mean(data):
     """Calculate the daily mean of a 2D inflammation data array.
-    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+    :param data: A 2D data array with inflammation data
+                (each row contains measurements for a single patient across all days).
     :returns: An array of mean values of measurements for each day.
     """
     return np.mean(data, axis=0)
@@ -28,7 +29,8 @@ def daily_mean(data):
 
 def daily_max(data):
     """Calculate the daily max of a 2D inflammation data array.
-    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+    :param data: A 2D data array with inflammation data
+                (each row contains measurements for a single patient across all days).
     :returns: An array of maximum values of measurements for each day.
     """
     return np.max(data, axis=0)
@@ -36,8 +38,31 @@ def daily_max(data):
 
 def daily_min(data):
     """Calculate the daily min of a 2D inflammation data array.
-    :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+    :param data: A 2D data array with inflammation data
+                (each row contains measurements for a single patient across all days).
     :returns: An array of minimum values of measurements for each day.
     """
     return np.min(data, axis=0)
 
+
+def patient_normalise(data):
+    """
+    Normalise patient data between 0 and 1 of a 2D inflammation data array.
+
+    Any NaN values are ignored, and normalised to 0
+
+    :param data: 2D array of inflammation data
+    :type data: ndarray
+
+    """
+    if not isinstance(data, np.ndarray):
+        raise TypeError('data input should be ndarray')
+    if len(data.shape) != 2:
+        raise ValueError('inflammation array should be 2-dimensional')
+    if np.any(data < 0):
+        raise ValueError('inflammation values should be non-negative')
+    max = np.nanmax(data, axis=1)
+    with np.errstate(invalid='ignore', divide='ignore'):
+        normalised = data / max[:, np.newaxis]
+    normalised[np.isnan(normalised)] = 0
+    return normalised
